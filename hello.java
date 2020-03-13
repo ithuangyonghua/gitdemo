89a8c082330e89aba9c37e13123731b66cf6749a
huangyonghua <ithuangyonghua@163.com>
2020-03-13 16:07:13 +0800

    111

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
     public static void main(String [] args){
-          System.out,println("GitHub Hello 1111111");
+          System.out,println("GitHub Hello 22222222");
+	   System.out.print("11");
 	  System.out.println("GitDemo Input");
           System.out.println("H");
     }
