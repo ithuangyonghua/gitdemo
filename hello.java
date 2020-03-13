56bcff0e92e01ba18417fee3b63a6754c003051d
huangyonghua <ithuangyonghua@163.com>
2020-03-13 16:43:20 +0800

    I am mynameishyh User

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String [] args){
           System.out,println("GitHub Hello 22222222");
-	   System.out.print("11");
+	   System.out.print("11   user --- name ");
           System.out,println("GitHub Hello 22222222`");
 	  System.out.println("Hello,This is My Project");
 	  System.out.println("GitDemo Input");
