4f060891abafaf8c4e3255dc4452cddb2aeb846b
=ithuangyonghua <=ithuangyonghua@163.com>
2020-03-13 15:49:50 +0800

    my Project

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
     public static void main(String [] args){
           System.out,println("GitHub Hello 22222222`");
+	  System.out.println("Hello,This is My Project");
 	  System.out.println("GitDemo Input");
     }
